9fb8382b7562e3f8a61c18338f8696f00704a346
Cecilia <cecilmutero66@gmail.com>
2024-06-18 19:30:14 -0700

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
-"""modularize my API"""
+"""Defines the status and stats endpoints for the API"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Defines the function to be called"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Returns the count of each object type"""
+    stats = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+    }
+    return jsonify(stats)
