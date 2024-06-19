7281b722c5456d2cfbc819a1c05553afb4721d5a
Cecilia <cecilmutero66@gmail.com>
2024-06-19 02:22:34 -0700

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,10 +25,17 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == '__main__':
     from os import getenv
     app.run(
             host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', 5000)),
-            threaded=True
+            threaded=True,
+            debug=False
         )
